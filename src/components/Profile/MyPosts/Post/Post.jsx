1da72219e55bf06a107691cdeba6e03ea1c26cae
Egor <egorholikov234@gmail.com>
2024-02-13 18:38:49 +0300

    added component folder

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
       <img src="https://cdn.fishki.net/upload/post/2022/09/02/4254307/tn/1269e66360feb39692e55e0e288f231b.jpg"/>
-      post1
+      {props.message}
       <div>
         <span>like</span>
       </div>
