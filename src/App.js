7ea24f0ab723d711f0960dda9466f633eb7bdc54
Egor <egorholikov234@gmail.com>
2024-02-14 17:57:24 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 
 const App = (props) => {
   return (
-    <div className="app-wrapper">
-      <Header/>
-      <Navbar/>
-      <div className="app-wrapper-content" >
-        {/*<Profile />*/}
-        <Dialogs/>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header/>
+        <Navbar/>
+          <div className="app-wrapper-content">
+            <Routes>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/dialogs" element={<Dialogs />}/>
+            </Routes>
+          </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
